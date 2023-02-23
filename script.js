55da686bc205a2d21504138773eb636aa56a7c09
Reyner Veliz <reyner@reynerveliz.com>
2023-02-23 08:30:34 -0600

    Local storage to save user data

--- script.js
+++ script.js
@@ -38,13 +38,27 @@ function switchTeme(event) {
     // console.log(event.target.checked);
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme','dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTeme);
+
+// Check LocalStorage for Theme
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if(currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
 
